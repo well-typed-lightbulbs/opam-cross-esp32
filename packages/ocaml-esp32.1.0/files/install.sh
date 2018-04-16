5b2173a48f2624df54a5f39f0b506aa5445e7a7e
Lucas Pluvinage <lucas.pluvinage@gmail.com>
2018-04-16 15:03:38 +0200

    fixed lwt package

--- packages/ocaml-esp32.1.0/files/install.sh
+++ packages/ocaml-esp32.1.0/files/install.sh
@@ -2,6 +2,10 @@
 
 PREFIX="$1"
 
+for pkg in bigarray bytes compiler-libs dynlink findlib graphics num num-top stdlib str threads unix; do
+  cp -r "${PREFIX}/lib/${pkg}" "${PREFIX}/esp32-sysroot/lib/"
+done
+
 ln -sf "${PREFIX}/bin/ocamlrun" "${PREFIX}/esp32-sysroot/bin/ocamlrun"
 mkdir -p "${PREFIX}/lib/findlib.conf.d"
 cp esp32.conf "${PREFIX}/lib/findlib.conf.d"    
