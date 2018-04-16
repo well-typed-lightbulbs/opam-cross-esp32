02dc953b1af311d2de8e08a4ee5dceaa7c3c98e2
Lucas Pluvinage <lucas.pluvinage@gmail.com>
2018-04-16 14:12:30 +0200

    ocaml for esp32 compilation update

--- packages/ocaml-esp32.1.0/files/install.sh
+++ packages/ocaml-esp32.1.0/files/install.sh
@@ -2,17 +2,6 @@
 
 PREFIX="$1"
 
-for pkg in bigarray bytes compiler-libs dynlink findlib graphics num num-top stdlib str threads unix; do
-  cp -r "${PREFIX}/lib/${pkg}" "${PREFIX}/esp32-sysroot/lib/"
-done
-
-
-ln -s ${PREFIX}/esp32-sysroot/bin/ocamlopt.byte ${PREFIX}/bin/ocamlopt-esp32
-cp ocamlc ${PREFIX}/bin/ocamlc-esp32
-cp tools/ocamlmklib ${PREFIX}/bin/ocamlmklib-esp32
-ln -s ${PREFIX}/bin/ocamlrun ${PREFIX}/esp32-sysroot/bin/ocamlrun
-
-cp byterun/libcamlrun.a ${PREFIX}$/esp32-sysroot/lib/
-
+ln -sf "${PREFIX}/bin/ocamlrun" "${PREFIX}/esp32-sysroot/bin/ocamlrun"
 mkdir -p "${PREFIX}/lib/findlib.conf.d"
 cp esp32.conf "${PREFIX}/lib/findlib.conf.d"    
