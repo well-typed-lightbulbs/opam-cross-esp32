b9b6c517837aadd50afc6e82d202185e56e8f1aa
Lucas Pluvinage <lucas.pluvinage@gmail.com>
2018-04-23 13:04:35 +0200

    A lot of package updates
    * Set myself as the maintainer
    * Set ocaml-esp32 dependency for most packages (as they are put in esp32-sysroot)
    * Worked on mirage library packages

---
+++ packages/ocaml-esp32.4.07/files/install.sh
@@ -0,0 +1,11 @@
+#!/bin/sh -e
+
+PREFIX="$1"
+
+for pkg in bigarray bytes compiler-libs dynlink findlib graphics num num-top stdlib str threads unix; do
+  cp -r "${PREFIX}/lib/${pkg}" "${PREFIX}/esp32-sysroot/lib/"
+done
+
+ln -sf "${PREFIX}/bin/ocamlrun" "${PREFIX}/esp32-sysroot/bin/ocamlrun"
+mkdir -p "${PREFIX}/lib/findlib.conf.d"
+cp esp32.conf "${PREFIX}/lib/findlib.conf.d"    
