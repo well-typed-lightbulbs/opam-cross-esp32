a859b4b6a4d7e3828a5710695e54699bef81b262
Lucas Pluvinage <lucas.pluvinage@gmail.com>
2018-05-08 16:19:23 +0200

    num is not installed twice anymore

--- packages/ocaml-esp32.4.06/files/install.sh
+++ packages/ocaml-esp32.4.06/files/install.sh
@@ -2,10 +2,10 @@
 
 PREFIX="$1"
 
-for pkg in bigarray bytes compiler-libs dynlink findlib graphics num num-top stdlib str threads unix; do
+for pkg in bigarray bytes compiler-libs dynlink findlib graphics stdlib str threads unix; do
   cp -r "${PREFIX}/lib/${pkg}" "${PREFIX}/esp32-sysroot/lib/"
 done
 sed -i -e 's/unix//g' ${PREFIX}/esp32-sysroot/lib/bigarray/META
 ln -sf "${PREFIX}/bin/ocamlrun" "${PREFIX}/esp32-sysroot/bin/ocamlrun"
 mkdir -p "${PREFIX}/lib/findlib.conf.d"
-cp esp32.conf "${PREFIX}/lib/findlib.conf.d"    
+cp esp32.conf "${PREFIX}/lib/findlib.conf.d"
